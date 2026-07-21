9df53bbec379663d8b8600cd3cfa86b70c3dfceb
muhibsalto8-hue <muhibsalto8@gmail.com>
2026-07-21 19:28:56 +0700

    Refactor global settings in settings.js

--- settings.js
+++ settings.js
@@ -37,10 +37,10 @@ global.fake = {
 }
 
 global.my = {
-	yt: "https://youtube.com/c/Nazedev",
-	gh: "https://github.com/nazedev",
-	gc: "https://chat.whatsapp.com/EqMTCcxdCZgHUJNl5KooCr",
-	ch: "120363250409960161@newsletter"
+	yt: "",
+	gh: "https://github.com/muhibsalto8-hue/GallehsBOTS",
+	gc: "",
+	ch: ""
 }
 
 global.limit = {
